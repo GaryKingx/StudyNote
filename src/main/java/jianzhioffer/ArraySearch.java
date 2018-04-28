e8d8caafdbd4eebdb01c17cf3b26bb23e3272972
GaryKingx <backonthehorse@163.com>
2018-04-28 18:36:51 +0800

    #注释

--- src/main/java/jianzhioffer/ArraySearch.java
+++ src/main/java/jianzhioffer/ArraySearch.java
@@ -1,5 +1,12 @@
 package jianzhioffer;
 
+/**
+ * 二维数组中的查找
+ * create y frank
+ * on 2018/04/28
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
+ */
 public class ArraySearch {
     public boolean Find(int target, int[][] array) {
         //数组行数
