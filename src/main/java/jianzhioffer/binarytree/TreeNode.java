b8b1bf52a4e9b26c4ec6ea53357147b5e7eb7f42
GaryKingx <backonthehorse@163.com>
2018-05-03 13:51:08 +0800

    #二叉树

---
+++ src/main/java/jianzhioffer/binarytree/TreeNode.java
@@ -0,0 +1,16 @@
+package jianzhioffer.binarytree;
+
+/**
+ * Ê÷½ÚµãÀà
+ * create by frank
+ * on 2018/05/03
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
